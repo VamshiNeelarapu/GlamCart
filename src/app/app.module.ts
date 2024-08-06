92b23abedd1c7a946d1299a7054a2b264a083fe8
Dey <suhdey@deloitte.com>
2024-08-06 07:28:25 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SubfooterComponent } from './footer/subfooter/subfooter.component';
+import { CopyrightComponent } from './footer/copyright/copyright.component';
+
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroSectionComponent,
+    SubfooterComponent,
+    CopyrightComponent,
+    SectionComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
